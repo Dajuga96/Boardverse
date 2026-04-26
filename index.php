b2a336256242b22e2131828e4898b2a82be882c7
Andreu Raga Català <anraca01@alumnatflorida.es>
2026-04-27 01:16:24 +0200

    Comentarios de index.php borrados

--- index.php
+++ index.php
@@ -8,23 +8,18 @@ $accion = $_GET['accion'] ?? 'index';
 
 switch ($accion) {
     case 'catalogo':
-        //Aquí podrías cargar un controlador específico para el catálogo
         $controller->catalogo();
         break;
     case 'producto':
-        //Aquí podrías cargar un controlador específico para el producto
         $controller->producto();
         break;
     case 'estrategia':
-        //Aquí podrías cargar un controlador específico para la estrategia
         $controller->index();
         break;
     case 'familiar':
-        //Aquí podrías cargar un controlador específico para la sección familiar
         $controller->index();
         break;
     case 'party':
-        //Aquí podrías cargar un controlador específico para la sección party
         $controller->index();
         break;
     default:
